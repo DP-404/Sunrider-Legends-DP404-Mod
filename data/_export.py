d54228462802df22e7ab5b13c830eaa423fbce35
DP-404 <73887377+DP-404@users.noreply.github.com>
2025-08-10 14:54:56 -0400

    Change _export.py to also export decoded files

--- data/_export.py
+++ data/_export.py
@@ -7,12 +7,14 @@ TEXT_EXPORTED = 'Exported: {Filename}'
 def export(filename:str):
     path = os.path.join(utility.DATA_PATH, filename)
     export_path = os.path.join(os.path.dirname(utility.DATA_PATH), filename)
+    export_path_decoded = os.path.join(os.path.dirname(utility.DATA_PATH), "Decoded_" + filename)
 
     text = utility.load_text(path)
     _,worklines = utility.split_text_lines(text)
     text = '\n'.join(worklines)
 
     utility.save_text(export_path, text, b64encode=True)
+    utility.save_text(export_path_decoded, text)
     print(TEXT_EXPORTED.format(Filename=filename))
 
 def export_all():
